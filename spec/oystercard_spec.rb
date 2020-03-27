fe07303a71bfc59070b853f42c375530c96a01ef
Harry Turnbull <hturnbull@live.co.uk>
2020-03-27 15:43:39 +0000

    minor spec cleanups

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,15 +1,18 @@
-require './lib/oystercard'
+require 'oystercard'
 
 describe Oystercard do
-  subject(:card) {Oystercard.new}
+
+  let(:edgeware) { double(:station) }
+  let(:waterloo) { double(:waterloo) }
+
 
   it 'oystercard has a balance of 0' do
-    expect(card).to have_attributes(:balance => 0)
+    expect(subject).to have_attributes(:balance => 0)
   end
 
   context '#top_up' do
     it 'oystercard responds to method top_up' do
-      expect(card).to respond_to(:top_up).with(1).argument
+      expect(subject).to respond_to(:top_up).with(1).argument
     end
   
     it 'can top up the balance' do
@@ -17,71 +20,56 @@ describe Oystercard do
     end
 
     it '#top_up fails if over balance' do
-      expect{ card.top_up(95) }.to raise_error "Over limit"
+      expect{ subject.top_up(Oystercard::CARD_LIMIT + 1) }.to raise_error "Over limit"
     end
-
-    it 'returns an empty list of journeys by default' do
-      expect(card.journeys).to eq []
-    end
-
-  end  
-
-
-
-context '#touch_in' do 
-    it 'start journey' do
-    expect(card).to respond_to(:touch_in).with(1).argument
-    card.top_up(1)
-    card.touch_in("edgeware")
-    expect(card.in_journey?).to eq true
   end
 
-  it 'will not allow card to touch in  and will throw an error  if have less than one pound in balance' do
-    expect {card.touch_in('edgeware')}.to raise_error "insufficient balance"
-  end 
-  it 'will save the  station to the entry_station on touch_in' do
-    card.top_up(1)
-    card.touch_in("edgeware")
-    expect(card.entry_station).to eq "edgeware"
-  end 
+  context '#touch_in' do 
+    it 'responds with 1 argument' do
+      expect(subject).to respond_to(:touch_in).with(1).argument
+    end
 
-  it 'touching in and out creates one journey' do
-    card.top_up(1)
-    card.touch_in("edgeware")
-    card.touch_out("waterloo")
-    expect(card.journeys).to eq [{:entry => "edgeware", :exit => "waterloo"}]
+    it 'starts journey' do
+      subject.top_up(1)
+      subject.touch_in(edgeware)
+      expect(subject.in_journey?).to eq true
+    end
+
+    it 'will not allow card to touch in and will throw an error if have less than one pound in balance' do
+      expect { subject.touch_in(edgeware) }.to raise_error "insufficient balance"
+    end 
+    it 'will save the station to the entry_station on touch_in' do
+      subject.top_up(1)
+      subject.touch_in(edgeware)
+      expect(subject.entry_station).to eq edgeware
+    end 
+
+    # testing state rather than behaviour, oops!
+    it 'touching in and out creates one journey' do
+      subject.top_up(1)
+      subject.touch_in(edgeware)
+      subject.touch_out(waterloo)
+      expect(subject.journey_list).to eq [{:entry => edgeware, :exit => waterloo}]
+    end
   end
-end
-
-
-
 
   context '#touch_out' do 
     it 'ends journey' do
-    expect(card).to respond_to(:touch_out)
-    expect(card.in_journey?).to eq false
+      expect(subject).to respond_to(:touch_out)
+      expect(subject.in_journey?).to eq false
     end 
 
     it 'deduct money from balance' do
-      card.top_up(10)
-      card.touch_in("edgeware")
-      expect{card.touch_out("waterloo")}.to change{ card.balance }.by (-1)
+      subject.top_up(10)
+      subject.touch_in(edgeware)
+      expect { subject.touch_out(waterloo) }.to change { subject.balance }.by (-1)
     end 
 
     it 'changes entry_station back to nil on touch_out' do
-      card.top_up(10)
-      card.touch_in('edgeware')
-      card.touch_out("waterloo")
-      expect(card.entry_station).to eq nil
+      subject.top_up(10)
+      subject.touch_in(edgeware)
+      subject.touch_out(waterloo)
+      expect(subject.entry_station).to eq nil
     end 
   end 
-
-
-
-    #expect { subject.deduct(1) }.to change{ subject.balance }.by (-1)
-      # what about if it goes under 0
-  
 end
-
-#Write a test that checks that an error is thrown if 
-#a card with insufficient balance is touched in
